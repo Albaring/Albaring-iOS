5da1204a9f4791762d933aafd7d9f9b42d671b9d
DAEJINLIM <daejin.limm@gmail.com>
2024-06-20 22:20:35 +0900

    Fitst Commit
    project setting

---
+++ Albaring/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Albaring
+//
+//  Created by 임대진 on 6/20/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .red
+    }
+
+
+}
+
